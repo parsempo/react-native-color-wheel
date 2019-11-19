e41656ab059e45b3f6f85a4ad2874452bc66482a
Hunaid Hassan <hhunaid@gmail.com>
2019-11-19 23:53:32 +0500

    feat: Add TS typings

---
+++ index.d.ts
@@ -0,0 +1,15 @@
+declare module 'react-native-color-wheel' {
+    import { PureComponent } from 'react'
+    import { StyleProp, ViewStyle } from 'react-native'
+
+    export type ColorWheelProps = {
+        initialColor?: string;
+        onColorChange?: (color: string) => void;
+        onColorChangeComplete?: (color: string) => void;
+        thumbSize?: number;
+        thumbStyle?: StyleProp<ViewStyle>;
+        style?: StyleProp<ViewStyle>;
+    }
+
+    export default class ColorWheel extends PureComponent<ColorWheelProps> {}
+}
